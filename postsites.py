ca086ccbec5ba971ba4b2b04fa3139b5909fb9fa
Benjamin Golder <benjamin.j.golder@gmail.com>
2011-05-19 10:20:48 -0700

    added to README, refined classes

--- postsites.py
+++ postsites.py
@@ -1,7 +1,42 @@
 """
 This module is intended to retrieve GeoJSON data for a particular site,
 organized into layers.
+
+Example Usage:
+
+    >>> config = ConfigurationInfo()
+    >>> config.layers = layer_dictionary
+    >>> config.siteLayer = 'parcels'
+    >>> config.terrainLayer = 'terrain_pts'
+    >>> config.sitePropertiesScript = 'myscript.py'
+    >>> config.distance = 100
+    >>> ds = DataSource(dbinfo)
+    >>> ds.configure(config)
+    >>> ds.getSiteJSON( id=200 )
+    {'Layers':[{'layer':'site', {'type':'Feature', 'geometry': {...}, 'properties':{...}}},
+            {'layer':'terrain', {'type':'FeatureCollection':[ ... ]}},
+            {'layer':'othersites', 'FeatureCollection':[ ... ]},
+            {'layer':'buildings', 'FeatureCollection':[ ... ]}],
+            'SiteProperties: {'prop0':'value0', ... }}
+    >>> ds.getSite( id=190 )
+    <Site object:'site 190'>
+    >>> s = Site(ds, id=200)
+    >>> s2 = Site(ds, query='assess_val = 3000')
+    >>> s.properties
+    None
+    >>> s.build() # retrieves the site data and builds
+    >>> s.properties
+    {'prop0':'value0', ... }
+    >>> s.layers
+    [<Layer object:'terrain'>, <Layer object:'buildings'>, ... ]
+    >>> s.prop0
+    'value0'
+    >>> layer = s.layers[2]
+    >>> layer.features
+    {'FeatureCollection':[ ... ]}
+
 """
+
 import os
 import psycopg2 as pg
 
@@ -25,6 +60,7 @@ class ConfigurationInfo(object):
         self.terrainLayer = None
         self.layerDict = None
         self.siteLayer = None
+        self.buildingLayer = None
         self.siteRadius = None
         self.sitePropertiesScript = None
 
@@ -61,24 +97,28 @@ class DataSource(object):
         self.dbname = dbinfo['dbname']
         self.dbpassword = ['password']
         self.dbinfo = dbinfo
-        self.layers = None
+        self.config = None
 
     def __unicode__(self):
         return 'dbname=%s' % self.dbname
 
     def getSiteJSON(self, id=None):
+        # connect to the database
+        self.connect()
+
         # For each layer
-        for layer in layers:
+
+        for layer in self.config.layers:
             # Make a call to the database
             # use the return values to populate
             # a Layer object
             pass
 
         # layer and use it to build up
-        # a site dictionary
+        # a site JSON
         siteDict = {}
 
-        pass
+        self.close()
 
     def viewLayers(self, filePath=None):
         if self.layer != None:
@@ -93,43 +133,10 @@ class DataSource(object):
         self.connection =  pg.connect(connString)
         return self.connection
 
+    def close(self):
+        self.connection.close()
 
-def test():
-    """
-    This is hard to conceptualize, but I've already done it.
-    Let's try to whip up some imagined usage examples and build from there.
 
-    >>> config = ConfigurationInfo()
-    >>> config.layers = layer_dictionary
-    >>> config.siteLayer = 'parcels'
-    >>> config.terrainLayer = 'terrain_pts'
-    >>> config.sitePropertiesScript = 'myscript.py'
-    >>> config.distance = 100
-    >>> ds = DataSource(dbinfo)
-    >>> ds.configure(config)
-    >>> ds.getSiteJSON( id=200 )
-    {'Layers':[{'layer':'site', {'type':'Feature', 'geometry': {...}, 'properties':{...}}},
-            {'layer':'terrain', {'type':'FeatureCollection':[ ... ]}},
-            {'layer':'othersites', 'FeatureCollection':[ ... ]},
-            {'layer':'buildings', 'FeatureCollection':[ ... ]}],
-            'SiteProperties: {'prop0':'value0', ... }}
-    >>> ds.getSite( id=190 )
-    <Site object:'site 190'>
-    >>> s = Site(ds, id=200)
-    >>> s2 = Site(ds, query='assess_val = 3000')
-    >>> s.properties
-    None
-    >>> s.build() # retrieves the site data and builds
-    >>> s.properties
-    {'prop0':'value0', ... }
-    >>> s.layers
-    [<Layer object:'terrain'>, <Layer object:'buildings'>, ... ]
-    >>> s.prop0
-    'value0'
-    >>> layer = s.layers[2]
-    >>> layer.features
-    {'FeatureCollection':[ ... ]}
-    """
 
 if __name__=='__main__':
 
@@ -144,7 +151,14 @@ if __name__=='__main__':
     config.layers = dictToLayers(layerDict)
     config.siteLayer = 'vacantparcels'
     config.terrainLayer = 'terrain'
+    config.buildingLayer = 'buildings'
 
 
     ds = DataSource(dbinfo)
+    ds.config = config
+    print 'site layer:', ds.config.siteLayer
+    print 'terrain layer:', ds.config.terrainLayer
+    print 'building layer:', ds.config.buildingLayer
+    for layer in ds.config.layers:
+        print layer.name
 
