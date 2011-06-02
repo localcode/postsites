7aba32c81b183cde9deabaf4daa5590cfb327c72
Benjamin Golder <benjamin.j.golder@gmail.com>
2011-06-01 18:13:54 -0700

    updated example

--- __init__.py
+++ __init__.py
@@ -1,38 +1,56 @@
 """
-This module is intended to retrieve GeoJSON data for a particular site,
-organized into layers.
+This module is meant to make the process of loading a bunch of shapefiles and
+then retrieving data on a per-site basis as painless as possible.
+The target audience for PostSites is amateur programmers/designers who are
+using scripts in 3d modeling programs or web stuff to import GIS data
+in GeoJSON format (a nice, readable, easy to parse format for GIS data that
+supports 3D). The package includes utilities that make it easy to load a large
+set of shapefiles into a PostGIS database, as well as classes that aid in configuring
+how site information is retrieved from that database. This module is intended to retrieve
+GeoJSON data for a particular site, organized into a set of layers.
 
-Example Usage:
+Dependencies:
+    PostSites is intended to work with PostGIS (hence the name), and
+    therefore expects that a PostGIS database is available, and that both
+    PostgreSQL and PostSites are installed. To make calls to the databse,
+    it uses psycopg2, and if using python 2.5 or earlier, it needs simplejson
+    as well. Additionally, it uses ogr2ogr and ogrinfo, commandline utilities included
+    with GDAL for loading and reading data. There are a couple of configuraiton and loading
+    scripts that utilize xlwt and xlrd. PostSites should work equally well on Windows
+    or Mac.
 
-    >>> config = ConfigurationInfo()
-    >>> config.layers = layer_dictionary
-    >>> config.siteLayer = 'parcels'
-    >>> config.terrainLayer = 'terrain_pts'
-    >>> config.distance = 100
-    >>> ds = DataSource(dbinfo)
-    >>> ds.configure(config)
-    >>> ds.getSiteJSON( id=200 )
-    {'Layers':[{'layer':'site', {'type':'Feature', 'geometry': {...}, 'properties':{...}}},
-            {'layer':'terrain', {'type':'FeatureCollection':[ ... ]}},
-            {'layer':'othersites', 'FeatureCollection':[ ... ]},
-            {'layer':'buildings', 'FeatureCollection':[ ... ]}],
-            'SiteProperties': {'prop0':'value0', ... }}
-    >>> ds.getSite( id=190 )
-    <Site object:'site 190'>
-    >>> s = Site(ds, id=200)
-    >>> s2 = Site(ds, query='assess_val = 3000')
-    >>> s.properties
-    None
-    >>> s.build() # retrieves the site data and builds
-    >>> s.properties
-    {'prop0':'value0', ... }
-    >>> s.layers
-    [<Layer object:'terrain'>, <Layer object:'buildings'>, ... ]
-    >>> s.prop0
-    'value0'
-    >>> layer = s.layers[2]
-    >>> layer.features
-    {'FeatureCollection':[ ... ]}
+Examples (disclaimer: still in development)
+
+First, we'll designate a folder that contains our shapefiles. PostSites will detect
+any shapefiles that inside this folder, even if they are nested into other folders.
+
+    >>> my_data_folder = 'C:\Users\myusername\Desktop\GIS data'
+
+The next command is a utility that makes it easy to configure how
+our shapefiles are loaded into the PostGIS database.
+It gives you one spreadsheet with all unique spatial reference systems
+and another sheet that lets you configure information about each file
+as well as letting you define any unknown projections. See the tutorials for
+examples of how to edit this file.
+
+    >>> import postsites
+    >>> f = postsites.makeXlsConfigurationFile(my_data_folder)
+    >>> f
+    'xls_config_GIS_data.xls' # the name of the resulting file.
+
+Once you have edited the 'xls_config_GIS_data.xls' file, you can use it to load
+everything into the database ...
+
+    >>> dbinfo = {'user':'me', 'dbname':'mydb', 'password':'s3cr3TP455w0rdZ'} # this is needed to connect to the db
+    >>> ds = postsites.loadFromXlsConfigurationFile( f, dbinfo ) # this may take a while, go get a coffee
+    >>> ds # ds is a DataSource object that we can use to retrieve and configure site information.
+    <postsites.DataSource object>
+
+Did you like that? Only three steps in order to get from a folder to a thing
+that can retrieve sites. Next, we can start getting GeoJSON data for a site like this:
+
+    >>> mysiteJson = ds.getSiteJson(id=203)
+
 """
 
 # Standard Library imports
